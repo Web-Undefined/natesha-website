087d67b9eea1718ef10c9591e1b0de8a9ff9c9c8
Kyle Butler <webundefinedcompany@gmail.com>
2026-06-14 21:52:03 -0700

    feat: add Cloudflare adapter for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import react from "@astrojs/react";
 import keystatic from "@keystatic/astro";
 import sitemap from "@astrojs/sitemap";
 
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
   site: "https://www.nateshadance.com",
   integrations: [react(), keystatic(), sitemap()],
+  adapter: cloudflare(),
 });
